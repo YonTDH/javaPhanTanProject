7eda1eb690172578f965172c5139a27af74f9c38
HNKhanh031 <hoangkhanh11223@gmail.com>
2025-04-16 17:02:18 +0700

    Update models

--- src/main/java/iuh/fit/models/CaLamViec.java
+++ src/main/java/iuh/fit/models/CaLamViec.java
@@ -17,7 +17,47 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-public class CaLamViec  {
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@ToString
+@Entity
+@Table(name = "CaLamViec")
+public class CaLamViec implements Serializable {
 
+    private static final long serialVersionUID = -7220260694252736743L;
 
+    @Id
+    @Column(name = "maCa", columnDefinition = "nvarchar(50)")
+    private String maCa;
+
+    @Column(name = "tenCa", columnDefinition = "nvarchar(255)")
+    private String tenCa;
+
+    @Column(name = "thoiGianBatDau", columnDefinition = "nvarchar(50)")
+    private String thoiGianBatDau;
+
+    @Column(name = "thoiGianKetThuc", columnDefinition = "nvarchar(50)")
+    private String thoiGianKetThuc;
+
+    @OneToMany(mappedBy = "caLamViec", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<NhanVien> dsNhanVien;
+
+    public CaLamViec(String maCa) {
+        this.maCa = maCa;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maCa);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        CaLamViec other = (CaLamViec) obj;
+        return Objects.equals(maCa, other.maCa);
+    }
 }
--- src/main/java/iuh/fit/models/NhomSanPham.java
+++ src/main/java/iuh/fit/models/NhomSanPham.java
@@ -1,4 +1,59 @@
 package iuh.fit.models;
 
-public class NhomSanPham  {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "NhomSanPham")
+@NamedQueries({
+        @NamedQuery(name = "NhomSanPham.getAll", query = "SELECT n FROM NhomSanPham n"),
+        @NamedQuery(name = "NhomSanPham.findByTen", query = "SELECT n FROM NhomSanPham n WHERE n.tenNhom = :tenNhom")
+})
+public class NhomSanPham implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "maNhom")
+    private String maNhom;
+
+    @Column(name = "tenNhom", columnDefinition = "nvarchar(255)")
+    private String tenNhom;
+
+    @OneToMany(mappedBy = "nhomSanPham", fetch = FetchType.LAZY)
+    private Set<SanPham> danhSachSanPham;
+
+    public NhomSanPham(String maNhom) {
+        this.maNhom = maNhom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NhomSanPham)) return false;
+        NhomSanPham that = (NhomSanPham) o;
+        return Objects.equals(maNhom, that.maNhom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maNhom);
+    }
+
+    @Override
+    public String toString() {
+        return "NhomSanPham{" +
+                "maNhom='" + maNhom + '\'' +
+                ", tenNhom='" + tenNhom + '\'' +
+                '}';
+    }
 }
