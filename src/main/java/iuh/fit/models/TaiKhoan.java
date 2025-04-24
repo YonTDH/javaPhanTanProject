0820025091519e97312e4f2b5e41ce21ae572990
dinhchung2102 <vodinhchung4@gmail.com>
2025-04-24 16:52:31 +0700

    update: account model & customer model

--- src/main/java/iuh/fit/models/TaiKhoan.java
+++ src/main/java/iuh/fit/models/TaiKhoan.java
@@ -1,7 +1,43 @@
 package iuh.fit.models;
 
-public class TaiKhoan  {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "TaiKhoan")
+public class TaiKhoan implements Serializable {
+
+    private static final long serialVersionUID = 2374252324184671506L;
+
+    @Id
+    @Column(name = "tenDangNhap", columnDefinition = "nvarchar(50)")
+    private String tenDangNhap;
+
+    @Column(name = "matKhau", columnDefinition = "nvarchar(255)", nullable = false)
+    private String matKhau;
+
+    @Column(name = "email", columnDefinition = "nvarchar(255)", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "chucVu")
+    private ChucVu chucVu;
+
+    @OneToOne(mappedBy = "taiKhoan")
+    private NhanVien nhanVien;
 }
 
